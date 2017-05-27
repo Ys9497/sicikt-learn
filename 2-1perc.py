9854781e92aef0c1ee6452e65d90eea66d718643
Ys9497 <y_s_9497@yahoo.co.jp>
2017-05-27 17:52:01 +0900

    Add files via upload

---
+++ 2-1perc.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#if文の条件には:
+#else:
+#pythonはindentを統一する必要がある
+import numpy as np
+
+def AND(x1, x2):
+    w1, w2, theta   = 0.5,  0.5,    0.7
+    tmp =   x1*w1+  x2*w2
+    #tmp = np.sum(w*x)
+    if tmp  <=  theta:
+        return 0
+    elif  tmp  >   theta:
+        return 1
+
+print(AND(0,0))
+
+def NAND(x1, x2):
+    x   =   np.array([x1,x2])
+    w   =   np.array([-0.5, -0.5])
+    b   =   0.7
+    tmp =   np.sum(x*w)+b
+    if tmp <= 0:
+            return 0
+    else:
+            return 1
+
+def OR(x1,  x2):
+    x   =   np.array([x1,x2])
+    w   =   np.array([1,1])
+    b   =   -0.1
+    tmp =   np.sum(x*w)+b
+    if tmp <= 0:
+            return 0
+    else:
+            return 1
+
+def XOR(x1  ,x2):
+    s1  =   NAND(x1,x2)
+    s2  =   OR(x1,x2)
+    y   =   AND(s1,s2)
+    return  y
+
+
+print(XOR(0,0))
+print(XOR(1,1))
